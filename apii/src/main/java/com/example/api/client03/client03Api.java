3fca7524f0b1b0869a36f27e0e3cbdeef997e747
Lenovo <18686155120@163.com>
2018-08-19 14:39:34 +0800

    3st commit branch_test2

--- apii/src/main/java/com/example/api/client03/client03Api.java
+++ apii/src/main/java/com/example/api/client03/client03Api.java
@@ -1,6 +1,7 @@
 package com.example.api.client03;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * Created by Lenovo on 2018/8/19.
@@ -10,4 +11,7 @@ public interface client03Api {
 
     @GetMapping("test_c3_get")
     public String test_c3_get();
+
+    @GetMapping("test_c3_post")
+    public String test_c3_post();
 }
